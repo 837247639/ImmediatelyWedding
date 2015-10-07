282c26010bbb1b652c7955f82754b71bc34eec64
lanou3g <lanou3g@lanou3gdeMac-mini-91.local>
2015-10-07 10:26:25 +0800

    新工程

---
+++ immediatelyWedding/immediatelyWedding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  immediatelyWedding
+//
+//  Created by lanou3g on 15/10/7.
+//  Copyright (c) 2015年 LiYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
